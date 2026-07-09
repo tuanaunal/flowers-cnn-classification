eb25de8ec56a143b9e76e297f2a47f83f4535651
Tuana <tuanaaunall10@gmail.com>
2026-07-09 16:11:10 +0300

    Veri seti yükleme ve örnek görselleştirme adımları tamamlandı

--- cnn.py
+++ cnn.py
@@ -3,6 +3,7 @@ flowers dataset:
     rgb: 224*224
 
 CNN ile siniflandirma modeli olusturma ve problemi cozme
+"""
 
 # import libraries
 from tensorflow_datasets import load # veri seti yukleme
@@ -27,8 +28,28 @@ import tensorflow as tf
 import matplotlib.pyplot as plt # gorsellestirme
 
 # veri seti yukleme
+(ds_train, ds_val), ds_info = load(
+    "tf_flowers", # veri seti ismi
+    split = ["train[:80%]", # veri setinin %80 i egitim icin
+             "train[80%:]"], # veri setinin %20 si test icin
+    as_supervised=True, # veri setinin gorsel, etiket ciftinin olmasi
+    with_info=True # veri seti hakkinda bilgi alma
+)
+
+print(ds_info.features) # veri seti hakkinda bilgi yazdirma
+print("Number of classes:", ds_info.features['label'].num_classes)
 
 # ornek veri gorsellestirme
+# egitim setinden rastgele 3 resim ve etiket alma
+fig = plt.figure(figsize = (10,5))
+for i, (image, label) in enumerate(ds_train.take(3)):
+    ax = fig.add_subplot(1, 3, i+1) # 1 satir, 3 sutun, i+1. resim
+    ax.imshow(image.numpy().astype("uint8")) # resmi gorsellestirme
+    ax.set_title(f"Etiket: {label.numpy()}") # etiket baslik olarak yazdirma
+    ax.axis("off") # eksenleri kapatma
+
+plt.tight_layout()
+plt.show() # grafigi gosterme
 
 # data augmentation + preprocessing
 
@@ -41,5 +62,3 @@ import matplotlib.pyplot as plt # gorsellestirme
 # traning
 
 # model evaluation
-
-"""
